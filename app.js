90ec7c705abea79c82d3dfc2860b87397416f4c5
uzebciukaite <urtezeb@gmail.com>
2022-09-27 18:34:33 +0300

    cors

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const mainRouter = require("./routes/mainRouter")
 require("dotenv").config();
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://mern-dating-site.herokuapp.com");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next()
 })
