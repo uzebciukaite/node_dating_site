746463d7c45ca61a0070929be3981bac80f5d5f5
uzebciukaite <urtezeb@gmail.com>
2022-09-27 19:18:34 +0300

    headers

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ require("dotenv").config();
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://extraordinary-biscotti-befba4.netlify.app");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Credentials", true)
 
    if (req.method === "OPTIONS") {
